2ec476d32aca64b1d9291d2a68cf77534050a8c5
barkli <153221572+KirilTeamCry@users.noreply.github.com>
2024-02-12 07:41:20 +0100

    Update LayerZeroOracleMock.js

--- deploy/LayerZeroOracleMock.js
+++ deploy/LayerZeroOracleMock.js
@@ -15,4 +15,21 @@ module.exports.skip = () =>
     })
 
 module.exports.tags = ["LayerZeroOracleMock", "test"]
+module.exports.dependencies = []module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    await deploy("LayerZeroOracleMock", {
+        // gasLimit:30000000,
+        from: deployer,
+        log: true,
+    })
+}
+
+module.exports.skip = () =>
+    new Promise(async (resolve) => {
+        resolve(false)
+    })
+
+module.exports.tags = ["LayerZeroOracleMock", "test"]
 module.exports.dependencies = []
